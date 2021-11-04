b58f0424d3b2f9cf306201efee472e2ea07b2259
Eduardo Binns <eduardo.binns.sanchez@gmail.com>
2021-11-04 16:50:28 -0600

    Read all the images saved within the project

--- textSentiment.py
+++ textSentiment.py
@@ -3,21 +3,23 @@ from azure.ai.textanalytics import TextAnalyticsClient
 import os
 
 credential = AzureKeyCredential("ef2f87788e5647a19b14cae72ad4f899")
-endpoint="https://textsentimental.cognitiveservices.azure.com/"
+endpoint = "https://textsentimental.cognitiveservices.azure.com/"
 text_analytics_client = TextAnalyticsClient(endpoint, credential)
 documents = []
+
 
 def analyzeFile(file):
     global documents
     text_file = open("./media/texts/" + file, "r", encoding="utf8")
-    analized_text = text_file.read()
+    parsed_text = text_file.read()
     text_file.close()
-    documents.append(analized_text)
+    documents.append(parsed_text)
 
-#Opening multiple files
+
+# Opening multiple files
 files = os.listdir("./media/texts")
 for f in files:
-    analyzeFile(f);
+    analyzeFile(f)
 
 response = text_analytics_client.analyze_sentiment(documents, language="en")
 result = [doc for doc in response if not doc.is_error]
@@ -29,4 +31,3 @@ for doc in result:
         doc.confidence_scores.neutral,
         doc.confidence_scores.negative,
     ))
-
